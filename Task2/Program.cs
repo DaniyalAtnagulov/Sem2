5cac88d397d70f9a72998104cd4eeb7acedc5b90
afromarine@afromarine-VirtualBox <dan@gmail.com>
2022-10-09 21:02:03 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Enter your number");
+int num=int.Parse(Console.ReadLine()); 
+int[] array= new int [num];
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+
+
