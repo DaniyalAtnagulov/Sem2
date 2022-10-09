ea40e60cbdec6242d0d376467620190304bde385
afromarine@afromarine-VirtualBox <dan@gmail.com>
2022-10-09 21:13:50 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,11 +6,11 @@
 
 // 32679 -> 6
 
-Console.WriteLine("Enter your number");
-int num=int.Parse(Console.ReadLine()); 
-int[] array= new int [num];
+Console.WriteLine("Enter your number"); 
 int n = int.Parse(Console.ReadLine());
 int k = (int)Math.Log10(n)-2;
 Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+
+
 
 
